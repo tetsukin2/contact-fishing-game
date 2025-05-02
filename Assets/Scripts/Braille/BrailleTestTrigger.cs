56026907faf78444faeacf2b6f3522e72e940c70
Joshcraft2002 <otwacraft@gmail.com>
2025-05-02 13:12:29 +0800

    joystick cursor

--- Assets/Scripts/Braille/BrailleTestTrigger.cs
+++ Assets/Scripts/Braille/BrailleTestTrigger.cs
@@ -3,13 +3,29 @@ using UnityEngine;
 
 public class BrailleTestTrigger : MonoBehaviour
 {
+    [SerializeField] private string _thumbPatternSequence;
+    [SerializeField] private string _indexPatternSequence;
+    [SerializeField] private bool _active = false;
+
     private bool lastPressedState = false;
 
-    private bool patternActive = false;
+    //private bool patternActive = false;
 
     void Update()
     {
         if (!InputDeviceManager.IsConnected) return;
+
+        //if (_active && !patternActive)
+        //{
+        //    patternActive = true;
+        //    BraillePatternPlayer.Instance.PlayPatternSequence(_thumbPatternSequence, BraillePatternPlayer.Finger.THUMB, true);
+        //    BraillePatternPlayer.Instance.PlayPatternSequence(_indexPatternSequence, BraillePatternPlayer.Finger.INDEX, true);
+        //}
+        //else if (!_active && patternActive)
+        //{
+        //    patternActive = false;
+        //    BraillePatternPlayer.Instance.StopPatternSequence(BraillePatternPlayer.Finger.BOTH);
+        //}
 
         // Only send when state changes
         if (InputDeviceManager.JoystickHeld != lastPressedState)
@@ -17,18 +33,18 @@ public class BrailleTestTrigger : MonoBehaviour
             lastPressedState = InputDeviceManager.JoystickHeld;
 
             //toggle
-            if (InputDeviceManager.JoystickHeld) patternActive = !patternActive;
+            //if (InputDeviceManager.JoystickHeld) patternActive = !patternActive;
 
-            if (patternActive)
-            {
-                BraillePatternPlayer.Instance.PlayPatternSequence("WaveOut", BraillePatternPlayer.Finger.THUMB, true);
-                //BraillePatternPlayer.Instance.PlayPatternSequence("WaveIn", BraillePatternPlayer.Finger.INDEX, true);
-            }
+            //if (patternActive)
+            //{
+            //    BraillePatternPlayer.Instance.PlayPatternSequence("WaveOut", BraillePatternPlayer.Finger.THUMB, true);
+            //    BraillePatternPlayer.Instance.PlayPatternSequence("WaveIn", BraillePatternPlayer.Finger.INDEX, true);
+            //}
                 
-            else
-            {
-                BraillePatternPlayer.Instance.StopPatternSequence(BraillePatternPlayer.Finger.BOTH);
-            }
+            //else
+            //{
+            //    BraillePatternPlayer.Instance.StopPatternSequence(BraillePatternPlayer.Finger.BOTH);
+            //}
                 
 
             //if (InputDeviceManager.JoystickHeld)
